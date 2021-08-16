0e1121e4437b10d55b48ce127bf4c927827c2c62
dogancandiktepe <dogan.diktepe@gmail.com>
2021-08-16 20:13:00 +0200

    express Internal Server Error Fixed v2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(resolve(process.cwd(), 'public')))
-    app.get('*', (req, res) => {
-        res.sendFile(resolve(process.cwd(), 'public/index.html'))
-    })
-}
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
 app.listen(PORT);
