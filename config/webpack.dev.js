ce1f1b3c79c815a37021660ce1ae05b732a672ea
dogancandiktepe <dogan.diktepe@gmail.com>
2021-08-16 19:52:41 +0200

    config folder

---
+++ config/webpack.dev.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './public',
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new ESLintPlugin(),
+    ],
+});
